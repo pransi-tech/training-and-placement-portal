f51f2a51c2829c1a1de3a51d0e5c27f5182585c0
Hridyaa Nayee <hridyaanayee369@gmail.com>
2026-07-29 14:53:40 +0530

    Added admin and student login pages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Home Page
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+// Admin Login Page
+Route::get('/admin/login', function () {
+    return view('admin.login');
+});
+
+
+// Student Login Page
+Route::get('/student/login', function () {
+    return view('student.login');
 });
